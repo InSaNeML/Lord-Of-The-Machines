ba4f87191659fba6f867a3d4a745814d575eb785
dJOKERdUMMY <avineshkumarcpl@gmail.com>
2018-04-01 15:12:33 +0530

    main.py updated

--- src/main.py
+++ src/main.py
@@ -5,6 +5,8 @@ import numpy as np
 #read the train.csv file
 print("Reading the data files.")
 df = pd.read_csv("./data/train.csv", parse_dates=['send_date'])
+test = pd.read_csv("./data/test.csv", parse_dates=['send_date'])
+
 #read the campaign_data.csv file
 daf = pd.read_csv("./data/campaign_data.csv")
 
@@ -15,10 +17,12 @@ print("Extracting time data.")
 #df["month"] = df.send_date.map(lambda x: x.strftime('%m'))
 #df["date"] = df.send_date.map(lambda x: x.strftime('%d'))
 df["hour"] = df.send_date.map(lambda x: x.strftime('%H'))
+test["hour"] = test.send_date.map(lambda x: x.strftime('%H'))
 #df["min"] = df.send_date.map(lambda x: x.strftime('%M'))
 
 #remove the send_date column after the data is extracted out , we dont need it anymore
 df.drop(["send_date"], axis=1, inplace=True)
+test.drop(["send_date"], axis=1, inplace=True)
 
 #design some label encoders as we don't have to be concerned with the data of the email in here
 from sklearn.preprocessing import LabelEncoder
@@ -47,7 +51,11 @@ daf.email_url = le_url.transform(daf.email_url)
 #merge both the dataframes on the "campaign_id" column 
 print("Merging dataframes.")
 final = pd.merge(df, daf, on='campaign_id')
+final_test = pd.merge(test, daf, on="campaign_id")
 
 #save this dataframe for direct future use
 print("Saving the dataframe to modified_train.csv")
 final.to_csv("./data/modified_train.csv", header=True, index=False)
+
+print("Saving the dataframe to modified_test.csv")
+final_test.to_csv("./data/modified_test.csv", header=True, index=False)
